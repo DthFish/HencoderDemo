81373d6ff736dc900de28bdfd6b0dfbdd8a8f7e6
DthFish <dthfish@163.com>
2018-07-16 15:52:49 +0800

    init PieChart AvatarView

---
+++ app/src/main/java/com/dthfish/hencoderdemo/view/AvatarView.java
@@ -0,0 +1,65 @@
+package com.dthfish.hencoderdemo.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.dthfish.hencoderdemo.R;
+import com.dthfish.hencoderdemo.Utils;
+
+/**
+ * Description
+ * Author zhaolizhi
+ * Date  2018/7/16.
+ */
+
+public class AvatarView extends View {
+    private static final float PADDING = Utils.dpToPx(15);
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private PorterDuffXfermode xfermode = new PorterDuffXfermode(PorterDuff.Mode.SRC_IN);
+
+    public AvatarView(Context context) {
+        super(context);
+    }
+
+    public AvatarView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AvatarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        float radius = Math.min(getWidth() / 2, getHeight() / 2) - PADDING;
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, radius, paint);
+
+        int save = canvas.saveLayer(0, 0, getWidth(), getHeight(), paint);
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, radius - PADDING, paint);
+        paint.setXfermode(xfermode);
+        canvas.drawBitmap(getAvatar((int) radius * 2), getWidth() / 2 - radius, getHeight() / 2 - radius, paint);
+        paint.setXfermode(null);
+
+        canvas.restoreToCount(save);
+
+    }
+
+    public Bitmap getAvatar(int width) {
+        BitmapFactory.Options option = new BitmapFactory.Options();
+        option.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), R.drawable.huaji, option);
+        option.inJustDecodeBounds = false;
+        option.inDensity = option.outWidth;
+        option.inTargetDensity = width;
+        return BitmapFactory.decodeResource(getResources(), R.drawable.huaji, option);
+    }
+}
--- app/src/main/java/com/dthfish/hencoderdemo/view/PieChart.java
+++ app/src/main/java/com/dthfish/hencoderdemo/view/PieChart.java
@@ -1,9 +1,15 @@
 package com.dthfish.hencoderdemo.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.dthfish.hencoderdemo.Utils;
 
 /**
  * Description
@@ -12,6 +18,16 @@ import android.view.View;
  */
 
 public class PieChart extends View {
+    private static final float PADDING = Utils.dpToPx(40);
+    private static final float OFFSET = Utils.dpToPx(10);
+
+    private float[] parts = {50, 70, 100, 80, 60};
+    private int[] colors = {Color.BLUE, Color.RED, Color.GREEN, Color.LTGRAY, Color.YELLOW};
+    private int pullOutIndex = 3;
+    private RectF rectF = new RectF();
+
+    private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
     public PieChart(Context context) {
         super(context);
     }
@@ -22,5 +38,31 @@ public class PieChart extends View {
 
     public PieChart(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        float radius = Math.min(getWidth() / 2, getHeight() / 2) - PADDING;
+        rectF.set(getWidth() / 2 - radius, getHeight() / 2 - radius,
+                getWidth() / 2 + radius, +getHeight() / 2 + radius);
+
+        float usedAngle = 0;
+        for (int i = 0; i < parts.length; i++) {
+            paint.setColor(colors[i]);
+            if (i == pullOutIndex) {
+                double angle = (usedAngle + parts[i] / 2) / 180 * Math.PI;
+                rectF.offset((float) Math.cos(angle) * OFFSET,
+                        (float) Math.sin(angle) * OFFSET);
+                canvas.drawArc(rectF, usedAngle, parts[i], true, paint);
+                rectF.offset(-(float) Math.cos(angle) * OFFSET,
+                        -(float) Math.sin(angle) * OFFSET);
+            } else {
+                canvas.drawArc(rectF, usedAngle, parts[i], true, paint);
+            }
+            usedAngle += parts[i];
+        }
+
     }
 }
