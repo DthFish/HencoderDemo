68e445a9a87ef18d5ba730d2abc0c3ed848f3a04
DthFish <dthfish@163.com>
2018-09-05 19:10:05 +0800

    update use Base64

--- app/src/main/java/com/dthfish/hencoderdemo/TinkManager.java
+++ app/src/main/java/com/dthfish/hencoderdemo/TinkManager.java
@@ -1,7 +1,7 @@
 package com.dthfish.hencoderdemo;
 
 import android.content.Context;
-import android.text.TextUtils;
+import android.util.Base64;
 import android.util.Log;
 
 import com.google.crypto.tink.Aead;
@@ -135,7 +135,8 @@ public class TinkManager {
                 Aead aead = AeadFactory.getPrimitive(mKeysetHandle);
 
                 byte[] ciphertext = aead.encrypt(plaintext.getBytes(), ASSOCIATED_DATA.getBytes());
-                emitter.onNext(toHex(ciphertext));
+
+                emitter.onNext(Base64.encodeToString(ciphertext, Base64.DEFAULT));
                 emitter.onComplete();
 
             }
@@ -182,7 +183,7 @@ public class TinkManager {
                 }
                 Aead aead = AeadFactory.getPrimitive(mKeysetHandle);
 
-                byte[] plaintext = aead.decrypt(toByte(ciphertext), ASSOCIATED_DATA.getBytes());
+                byte[] plaintext = aead.decrypt(Base64.decode(ciphertext, Base64.DEFAULT), ASSOCIATED_DATA.getBytes());
                 emitter.onNext(new String(plaintext));
                 emitter.onComplete();
 
@@ -216,44 +217,5 @@ public class TinkManager {
 
                     }
                 });
-    }
-
-    public static byte[] toByte(String hexString) {
-        if (TextUtils.isEmpty(hexString))
-            return null;
-        hexString = hexString.toLowerCase();
-        final byte[] byteArray = new byte[hexString.length() >> 1];
-        int index = 0;
-        for (int i = 0; i < hexString.length(); i++) {
-            if (index > hexString.length() - 1)
-                return byteArray;
-            byte highDit = (byte) (Character.digit(hexString.charAt(index), 16) & 0xFF);
-            byte lowDit = (byte) (Character.digit(hexString.charAt(index + 1), 16) & 0xFF);
-            byteArray[i] = (byte) (highDit << 4 | lowDit);
-            index += 2;
-        }
-        return byteArray;
-    }
-
-
-    /**
-     * byte[] to Hex string.
-     *
-     * @param byteArray the byte array
-     * @return the string
-     */
-    public static String toHex(byte[] byteArray) {
-        final StringBuilder hexString = new StringBuilder("");
-        if (byteArray == null || byteArray.length <= 0)
-            return null;
-        for (int i = 0; i < byteArray.length; i++) {
-            int v = byteArray[i] & 0xFF;
-            String hv = Integer.toHexString(v);
-            if (hv.length() < 2) {
-                hexString.append(0);
-            }
-            hexString.append(hv);
-        }
-        return hexString.toString().toLowerCase();
     }
 }
